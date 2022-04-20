b5646abaf23f6c3b3adadc51aca7c1e12e1a1741
templar-command0 <ajay.kumar@talentelgia.in>
2022-04-20 12:34:23 +0530

    commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Welcome, this is a notes app")
+
 let notesObj = [];
 showNotes();
 // if user adds a note , add it to the local storage
@@ -16,13 +17,14 @@ addBtn.addEventListener('click', function(e){
         notesObj.push(addText.value);
         localStorage.setItem("notes", JSON.stringify(notesObj))
         addText.value = "";
-        console.log(notesObj);
+        // console.log(notesObj);
         showNotes();
         }
     })
 
 function showNotes(){
     let notes = localStorage.getItem("notes")
+    
     if(notes == null){
         notesObj = []
     }else{
@@ -35,7 +37,7 @@ function showNotes(){
         <div class="card-body">
           <h5 class="card-title">Note ${index + 1}</h5>
           <p class="card-text">${element}</p>
-          <button class="btn btn-primary">Delete Note</button>
+          <button id = "${index}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
         </div>
                 `
@@ -49,3 +51,52 @@ function showNotes(){
         notesElm.innerHTML = `Nothing to show here , use "Add Note" section to add notes`
     }
 }
+
+
+// function to delete a note
+
+function deleteNote(index){
+    let notes = localStorage.getItem("notes")
+    // console.log("I am deleting",index);
+    
+    if(notes == null){
+        notesObj = []
+    }else{
+        notesObj = JSON.parse(notes)
+    }
+
+    notesObj.splice(index,1)
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes()
+}
+
+// adding search functionality
+
+const searcH = document.getElementById('search-txt');
+
+searcH.addEventListener("input", function(){
+    const searchTxt = searcH.value.toLowerCase()
+    const noteCards = document.getElementsByClassName("noteCard")
+
+    Array.from(noteCards).forEach(function(element){
+
+        let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase()
+        if(cardTxt.includes(searchTxt)){
+            element.style.display = "block"
+        }else{
+            element.style.display = "none"
+        }
+    })
+
+    // console.log("Input event fired",searchTxt)
+})
+
+
+
+/*
+further features
+1.Add Custom title to noteCard
+2. Mark a note as important
+3. separate notes by user
+4. sync with server and host
+5. Add edit note option
